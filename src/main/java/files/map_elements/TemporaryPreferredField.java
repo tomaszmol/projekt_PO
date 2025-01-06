75b274d877c520a87243b86ecb5b6b346e7b29ed
tomaszmol <tmollegro@gmail.com>
2025-01-06 20:46:04 +0100

    dodanie TemporaryPreferredField do obsługi czasowo preferowanych pol, dla życiodajnych trucheł

---
+++ src/main/java/files/map_elements/TemporaryPreferredField.java
@@ -0,0 +1,9 @@
+package files.map_elements;
+
+import files.util.Vector2d;
+
+public class TemporaryPreferredField extends PreferredField {
+    public TemporaryPreferredField(Vector2d position) {
+        super(position);
+    }
+}
