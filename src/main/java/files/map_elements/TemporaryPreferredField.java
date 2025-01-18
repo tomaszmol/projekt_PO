8ec873ba10a09135eeaca310b0e101372e3425ad
Tomasz Mol <165932863+tomaszmol@users.noreply.github.com>
2025-01-18 01:07:03 +0100

    Revert "pull request"

--- src/main/java/files/map_elements/TemporaryPreferredField.java
+++ src/main/java/files/map_elements/TemporaryPreferredField.java
@@ -4,30 +4,13 @@ import files.util.Vector2d;
 import javafx.scene.paint.Color;
 
 public class TemporaryPreferredField extends PreferredField {
-    private int daysToLive;
-    int daysCounter = 0;
-    public TemporaryPreferredField(Vector2d position, int daysToLive) {
+    public TemporaryPreferredField(Vector2d position) {
         super(position);
-        this.daysToLive = daysToLive;
-    }
-
-    public void incrementDaysCounter() {
-        daysCounter++;
     }
 
     @Override
     public Color getElementColour() {
-        return Color.LAWNGREEN;
+        return Color.DARKGREEN;
     }
 
-    public int getDaysToLive() {
-        return daysToLive;
-    }
-
-    public boolean isTooOld() {
-        if (daysCounter >= daysToLive) {
-            return true;
-        }
-        return false;
-    }
 }
