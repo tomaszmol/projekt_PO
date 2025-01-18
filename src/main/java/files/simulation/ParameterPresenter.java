9bfc019175aaf8c03831eaa8988ca687a055b15a
DominikMat <dominikmat@student.agh.edu.pl>
2025-01-18 18:21:59 +0100

    walidacja poczatkowych parametrow

--- src/main/java/files/simulation/ParameterPresenter.java
+++ src/main/java/files/simulation/ParameterPresenter.java
@@ -1,6 +1,5 @@
 package files.simulation;
 
-
 import files.map_elements.StatisticsTracker;
 import files.maps.WorldMap;
 import files.maps.variants.FullPredestinationEquatorMap;
@@ -12,9 +11,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ParameterPresenter {
 
@@ -56,73 +59,124 @@ public class ParameterPresenter {
     @FXML
     public CheckBox fullPredestinationFlag;
 
-    @FXML
-    public void onSimulationStartClicked() {
-            try {
-                // initialize new fxml presenter
-                FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("simulationWindow.fxml"));
-                BorderPane newSimulationRoot = loader.load();
-                SimulationPresenter newPresenter = loader.getController();
-
-                // get all data
-                params = getSimulationParams();
-                StatisticsTracker tracker = new StatisticsTracker();
-
-                // select simulation map
-                WorldMap map = selectSimulationMap();
-
-                // start new simulation thread
-                Thread simulationThread = new Thread(() -> {
-                    try {
-                        Simulation newSimulation = new Simulation(params, map, tracker);
-                        newPresenter.setSimulationData(params, map, tracker, newSimulation);
-                        newSimulation.run();
-                    } catch (Exception e) {
-                        System.err.println("Simulation thread error! + " + e.getMessage());
-                    }
-                });
-                simulationThread.setDaemon(true);
-                simulationThread.start();
-
-
-                // Tworzenie nowego okna
-                Stage newStage = new Stage();
-                newStage.setScene(new Scene(newSimulationRoot));
-                newStage.setTitle("Simulation Window #" + simulationCount++);
-                newStage.show();
-
-            } catch (IOException e) {
-                System.err.println("Simulation failed to initialize! + " + e.getMessage());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-    }
+    private final Map<TextField, double[]> fieldConstraints = new HashMap<>();
 
     @FXML
     public void initialize() {
-        // Ustawianie wartości domyślnych
-        mapHeightField.setText("10");
-        mapWidthField.setText("10");
+        // Ustawianie wartości domyślnych i zakresu
+        mapHeightField.setText("16");
+        fieldConstraints.put(mapHeightField, new double[]{1, 50});
+
+        mapWidthField.setText("16");
+        fieldConstraints.put(mapWidthField, new double[]{1, 50});
+
         plantEnergyField.setText("20");
+        fieldConstraints.put(plantEnergyField, new double[]{0, 1000});
+
         copulationEnergyField.setText("15");
+        fieldConstraints.put(copulationEnergyField, new double[]{0, 1000});
+
         initialAnimalEnergyField.setText("100");
+        fieldConstraints.put(initialAnimalEnergyField, new double[]{0, 1000});
+
         energyCostPerMoveField.setText("1");
-        initialAnimalsField.setText("3");
-        dailyPlantSpawnsField.setText("5");
+        fieldConstraints.put(energyCostPerMoveField, new double[]{0, 1000});
+
+        initialAnimalsField.setText("10");
+        fieldConstraints.put(initialAnimalsField, new double[]{0, 100});
+
+        dailyPlantSpawnsField.setText("4");
+        fieldConstraints.put(dailyPlantSpawnsField, new double[]{0, 100});
+
         simulationSteps.setText("100");
+        fieldConstraints.put(simulationSteps, new double[]{1, 10000});
+
         geneMutationChance.setText("0.02");
+        fieldConstraints.put(geneMutationChance, new double[]{0, 1});
+
         geneNumber.setText("6");
+        fieldConstraints.put(geneNumber, new double[]{1, 20});
+
+        initialPlantNumberField.setText("50");
+        fieldConstraints.put(initialPlantNumberField, new double[]{0, 1000});
+
+        waitingTimeBetweenMoves.setText("100");
+        fieldConstraints.put(waitingTimeBetweenMoves, new double[]{10, 500});
+
         oldnessSadnessFlag.setSelected(false);
         liveGivingCorpseFlag.setSelected(false);
         fullPredestinationFlag.setSelected(true);
         equatorFlag.setSelected(true);
-        waitingTimeBetweenMoves.setText("100");
-        initialPlantNumberField.setText("10");
+
+        // Definiowanie zakresów dla pól
     }
 
-    SimulationParams getSimulationParams(){
+    @FXML
+    public boolean validateInputs() {
+        // Walidacja wartości pól tekstowych
+        AtomicBoolean valid = new AtomicBoolean(true);
+        fieldConstraints.forEach((field, range) -> {
+            try {
+                double value = Double.parseDouble(field.getText().trim());
+                if (value < range[0] || value > range[1]) {
+                    field.setStyle("-fx-border-color: red;"); // Podświetlenie na czerwono
+                    valid.set(false);
+                } else {
+                    field.setStyle(""); // Usunięcie stylu błędu
+                }
+            } catch (NumberFormatException e) {
+                field.setStyle("-fx-border-color: red;"); // Podświetlenie dla niepoprawnych danych
+            }
+        });
+        return valid.get();
+    }
+
+    @FXML
+    public void onSimulationStartClicked() {
         try {
-            // Pobieranie danych liczbowych z pól tekstowych
+            if (!validateInputs()) {return;}
+
+            // Initialize new FXML presenter
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("simulationWindow.fxml"));
+            BorderPane newSimulationRoot = loader.load();
+            SimulationPresenter newPresenter = loader.getController();
+
+            // Get all data
+            params = getSimulationParams();
+            StatisticsTracker tracker = new StatisticsTracker();
+
+            // Select simulation map
+            WorldMap map = selectSimulationMap();
+
+            // Start new simulation thread
+            Thread simulationThread = new Thread(() -> {
+                try {
+                    Simulation newSimulation = new Simulation(params, map, tracker);
+                    newPresenter.setSimulationData(params, map, tracker, newSimulation);
+                    newSimulation.run();
+                } catch (Exception e) {
+                    System.err.println("Simulation thread error! + " + e.getMessage());
+                }
+            });
+            simulationThread.setDaemon(true);
+            simulationThread.start();
+
+            // Tworzenie nowego okna
+            Stage newStage = new Stage();
+            newStage.setScene(new Scene(newSimulationRoot));
+            newStage.setTitle("Simulation Window #" + simulationCount++);
+            newStage.show();
+
+        } catch (IOException e) {
+            System.err.println("Simulation failed to initialize! + " + e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    SimulationParams getSimulationParams() {
+        try {
+            // Pobieranie danych z pól tekstowych
             int mapHeight = Integer.parseInt(mapHeightField.getText().trim());
             int mapWidth = Integer.parseInt(mapWidthField.getText().trim());
             int plantEnergyProfit = Integer.parseInt(plantEnergyField.getText().trim());
@@ -137,7 +191,6 @@ public class ParameterPresenter {
             int waitingTime = Integer.parseInt(waitingTimeBetweenMoves.getText().trim());
             int initialPlantNumber = Integer.parseInt(initialPlantNumberField.getText().trim());
 
-            // Pobieranie wartości boolean z checkboxów
             boolean fullPredestinationFlagValue = fullPredestinationFlag.isSelected();
             boolean oldnessSadnessFlagValue = oldnessSadnessFlag.isSelected();
             boolean equatorFlagValue = equatorFlag.isSelected();
@@ -148,7 +201,7 @@ public class ParameterPresenter {
                     mapHeight, mapWidth,
                     plantEnergyProfit, minCopulationEnergy, initialAnimalEnergy,
                     energyCostPerMove, initialAnimalsOnMap, dailyPlantSpawns, fullPredestinationFlagValue,
-                    oldnessSadnessFlagValue, equatorFlagValue, liveGivingCorpseFlagValue,simSteps,
+                    oldnessSadnessFlagValue, equatorFlagValue, liveGivingCorpseFlagValue, simSteps,
                     mutationChance, geneNum, waitingTime, initialPlantNumber
             );
         } catch (NumberFormatException e) {
@@ -158,34 +211,15 @@ public class ParameterPresenter {
     }
 
     WorldMap selectSimulationMap() throws Exception {
-        if (params.fullPredestinationFlag() &&  params.equatorFlag()) return new FullPredestinationEquatorMap(params);
-        if (params.fullPredestinationFlag() && params.liveGivingCorpseFlag()) return new FullPredestinationLiveGivingCorpseMap(params);
-        if ( params.oldnessSadnessFlag() &&  params.equatorFlag())  return new OldnessSadnessEquatorMap(params);
-        if ( params.oldnessSadnessFlag() && params.liveGivingCorpseFlag()) return new OldnessSadnessLiveGivingCorpseMap(params);
+        if (params.fullPredestinationFlag() && params.equatorFlag())
+            return new FullPredestinationEquatorMap(params);
+        if (params.fullPredestinationFlag() && params.liveGivingCorpseFlag())
+            return new FullPredestinationLiveGivingCorpseMap(params);
+        if (params.oldnessSadnessFlag() && params.equatorFlag())
+            return new OldnessSadnessEquatorMap(params);
+        if (params.oldnessSadnessFlag() && params.liveGivingCorpseFlag())
+            return new OldnessSadnessLiveGivingCorpseMap(params);
 
         throw new Exception("No Map Selected!");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
